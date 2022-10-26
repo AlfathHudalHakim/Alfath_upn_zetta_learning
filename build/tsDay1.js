c59039aad942b9ad691eb5b80e70d4f63e643670
Frosszzz <alfathhudalhakim123@gmail.com>
2022-10-27 03:29:08 +0700

    adding_components

---
+++ build/tsDay1.js
@@ -0,0 +1,8 @@
+"use strict";
+function argumentsDay1(a, b, c) {
+    return a.slice(b, c);
+}
+const a = "Learning Typescript is different than Javascript";
+const b = 9;
+const c = 19;
+console.log(argumentsDay1(a, b, c));
