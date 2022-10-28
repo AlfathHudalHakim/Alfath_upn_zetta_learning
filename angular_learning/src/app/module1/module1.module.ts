2f98f1cec7e64b7f8f88e82a9acca030ed1287c5
Frosszzz <alfathhudalhakim123@gmail.com>
2022-10-28 11:14:40 +0700

    initialize module, and update components

---
+++ angular_learning/src/app/module1/module1.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Module1Component } from './module1.component';
+
+
+
+@NgModule({
+  declarations: [
+    Module1Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Module1Module { }
---
+++ angular_learning/src/app/module2/module2.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Module2Component } from './module2.component';
+
+
+
+@NgModule({
+  declarations: [
+    Module2Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Module2Module { }
