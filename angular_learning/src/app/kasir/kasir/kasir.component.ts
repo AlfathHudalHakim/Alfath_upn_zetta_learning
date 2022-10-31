3385a7c7601f420ddae098949f235c37645cebd9
Frosszzz <alfathhudalhakim123@gmail.com>
2022-10-31 13:19:45 +0700

    chages, not done

--- angular_learning/src/app/kasir/kasir/kasir.component.ts
+++ angular_learning/src/app/kasir/kasir/kasir.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kasir.component.scss']
 })
 export class KasirComponent implements OnInit {
+  public games: Game[] = [
+    {id:'1', name: 'gta v', price: 500000},
+    {id:'2', name: 'Assassins Creed', price: 450000},
+    {id:'3', name: 'the witcher', price: 820000},
+    {id:'4', name: 'far cry', price: 550000},
+    {id:'5', name: 'fallout', price: 850000},
+    {id:'6', name: 'fifa 23', price: 760000},
+    {id:'7', name: 'DmC 5', price: 480000},
+    {id:'8', name: 'spiderman', price: 800000},
+  ]
+  public selectedGames: SelectedGame[]
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
--- angular_learning/src/app/kasir/menu/menu.component.ts
+++ angular_learning/src/app/kasir/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  @Input('games') games: game[] = []
+  @Output('gameAdded') onAddGame: EventEmitter<game> = new EventEmitter<game>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addGame(game: Game):void {
+    this.onAddGame.emit(game)
   }
 
 }
