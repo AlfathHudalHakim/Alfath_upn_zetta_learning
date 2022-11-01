74520a9a6c97ef32eba70b48c0e58291d48dbd0d
Frosszzz <alfathhudalhakim123@gmail.com>
2022-11-01 22:08:04 +0700

    initialize day 3

---
+++ angular_learning/src/app/kasir/bayar/bayar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bayar',
+  templateUrl: './bayar.component.html',
+  styleUrls: ['./bayar.component.scss']
+})
+export class BayarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
