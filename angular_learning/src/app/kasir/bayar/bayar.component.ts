75c0cce7aa288b527b82efd71e16a435e44f2a25
Frosszzz <alfathhudalhakim123@gmail.com>
2022-11-08 22:27:04 +0700

    adding service

--- angular_learning/src/app/kasir/bayar/bayar.component.ts
+++ angular_learning/src/app/kasir/bayar/bayar.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit, Input, Output, EventEmitter, AfterContentChecked } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { map, Observable, pipe } from 'rxjs';
+import { kasirService, selectedGame } from '../kasir.service';
 import { Game } from '../kasir/kasir.component';
-import { selectedGame } from '../kasir/kasir.component';
+
 @Component({
   selector: 'app-bayar',
   templateUrl: './bayar.component.html',
   styleUrls: ['./bayar.component.scss']
 })
 export class BayarComponent implements OnInit, AfterContentChecked {
-  @Output ('gameAdded') onAddGame : EventEmitter<Game> = new EventEmitter<Game>()
-  @Input() games!: selectedGame[];
 
-  public total:number = 0;
+  public games : Observable<selectedGame[]>
+  public total : Observable<number>
 
-  constructor() {
+  constructor(private KasirService: kasirService) {
     console.log('bayar constructor loaded')
+
+    this.games = this.KasirService.selectedGames$
+    this.total = this.KasirService.selectedGames$.pipe(
+      map((games) => games.reduce((total, game) => total += game.jumlah * game.harga , 0)))
    }
 
   ngOnInit(): void {
@@ -21,7 +26,9 @@ export class BayarComponent implements OnInit, AfterContentChecked {
   }
 
   ngAfterContentChecked(): void {
-    this.total = this.games.reduce((total, game) => total += game.jumlah * game.harga , 0)
-  }
+    this.total = this.KasirService.selectedGames$.pipe(
+      map((games) => games.reduce((total, game) => total += game.jumlah * game.harga , 0)))
+   }
+
 
 }
